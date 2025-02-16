e8a31899b8c7eb93e35f1df5eaeac3f9baa347eb
Bastian Schwab <writetobastian@gmail.com>
2025-02-16 19:55:31 +0100

    neue Datei: Dashboard Classes.py neue Datei: Testing.py

---
+++ Testing.py
@@ -0,0 +1,6 @@
+class University:
+    def __init__(self, university_name, university_location):
+        self.university_name = university_name
+        self.university_location = university_location
+
+    
